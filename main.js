5f2c4fff008f431bee9241bbe823ed9b6dcb0341
Niemand <leo.binz.2805@gmail.com>
2025-06-08 19:37:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let highscore = localStorage.getItem("highscore") || 0;
 
 const button = document.getElementById("mainButton");
 const highscoreDisplay = document.getElementById("highscore");
+const message = document.getElementById("message");
 
 highscoreDisplay.textContent = highscore;
 
@@ -11,14 +12,20 @@ button.addEventListener("click", () => {
   const random = Math.random() * 100;
 
   if (random < chance) {
-    alert("Game Over!");
+    // Game Over
+    message.textContent = "Game Over! Punkte zurückgesetzt.";
     score = 0;
     chance = 0;
     button.textContent = score;
+
+    setTimeout(() => {
+      message.textContent = "";
+    }, 2000);
   } else {
     score++;
     chance++;
     button.textContent = score;
+    message.textContent = "";
 
     if (score > highscore) {
       highscore = score;
